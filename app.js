7f18b8948f31e79e642039db2f20132de6d59115
hakeemadnan <hakeemadnan86@gmail.com>
2026-05-06 08:28:41 +0530

    authentication-using npm passport

--- app.js
+++ app.js
@@ -11,10 +11,14 @@ const session =require("express-session");
 // const {listingSchema, reviewSchema} = require("./schema.js");
 // const Review = require("./models/review.js")
 const flash = require("connect-flash")
+const passport = require("passport");
+const localStrategy = require("passport-local")
+const User = require("./models/user.js");
 
-
-const listings = require("./routes/listings.js");
-const reviews = require("./routes/review.js")
+const listingRouter = require("./routes/listings.js");
+const reviewsRouter = require("./routes/review.js")
+const usersRouter = require("./routes/user.js");
+const { setupPrimary } = require('cluster');
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -51,19 +55,34 @@ const sessionOptions ={
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next) =>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
-
+app.get("/demouser",async(req,res) =>{
+    let fakeUser = new User({
+        email:"student@gmail.com",
+        username :"newstudent"
+    });
+    const registeredUser = await User.register(fakeUser,"helloWorld");
+    res.send(registeredUser);
+})
 app.get("/",(req,res)=>{
     res.send("Hi , I am root");
 });
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
-
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewsRouter);
+app.use("/",usersRouter);
 // app.get("/testListing",async (req,res)=>{
 //     let SampleListing =new Listing({
 //         title:"My Villa",
